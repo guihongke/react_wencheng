9655c24793dc92657c85a5d1916615021068b42a
Kegh <guihong_ke@163.com>
2018-08-01 21:56:25 +0800

    update： 增加侧边菜单

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage';
+import Main from './components/SiderMenu/SiderMenu';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={IndexPage} />
+        <Route path="/main" exact component={Main} />
       </Switch>
     </Router>
   );
